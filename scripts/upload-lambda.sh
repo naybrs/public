4b5e183baa1c26ad138ee6071c8a0f75c6825777
Adam P. Wright <a@naybrs.com>
2024-07-15 09:46:36 -0700

    added production to main branch alias name

--- scripts/upload-lambda.sh
+++ scripts/upload-lambda.sh
@@ -23,7 +23,7 @@ fi
 AWS_PAGER="" aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://dist/index.zip --region $REGION
 
 # Check if the alias already exists for the function in the specified region
-ALIAS_EXISTS=$(aws lambda get-alias --function-name $FUNCTION_NAME --name $BRANCH_NAME --region $REGION 2>&1)
+ALIAS_EXISTS=$(aws lambda get-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME --region $REGION 2>&1)
 
 # If the alias does not exist, create it pointing to the latest version
 # Otherwise, update the existing alias to point to the latest version
@@ -31,13 +31,13 @@ if [[ $ALIAS_EXISTS == *"ResourceNotFoundException"* ]]; then
     # Get the latest version number
     LATEST_VERSION=$(aws lambda publish-version --function-name $FUNCTION_NAME --region $REGION | jq -r '.Version')
     # Create a new alias for the latest version in the specified region
-    aws lambda create-alias --function-name $FUNCTION_NAME --name $BRANCH_NAME --function-version $LATEST_VERSION --region $REGION
-    echo "Created new alias '$BRANCH_NAME' for function '$FUNCTION_NAME' version '$LATEST_VERSION' in region '$REGION'"
+    aws lambda create-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME --function-version $LATEST_VERSION --region $REGION
+    echo "Created new alias '$ALIAS_NAME' for function '$FUNCTION_NAME' version '$LATEST_VERSION' in region '$REGION'"
 else
     # Update the existing alias to point to the latest version in the specified region
     LATEST_VERSION=$(aws lambda publish-version --function-name $FUNCTION_NAME --region $REGION | jq -r '.Version')
-    aws lambda update-alias --function-name $FUNCTION_NAME --name $BRANCH_NAME --function-version $LATEST_VERSION --region $REGION
-    echo "Updated alias '$BRANCH_NAME' for function '$FUNCTION_NAME' to version '$LATEST_VERSION' in region '$REGION'"
+    aws lambda update-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME --function-version $LATEST_VERSION --region $REGION
+    echo "Updated alias '$ALIAS_NAME' for function '$FUNCTION_NAME' to version '$LATEST_VERSION' in region '$REGION'"
 fi
 
 # Self-delete the script
